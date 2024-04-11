ef2e01b6b45b7944dd61316c42d06f32c4876b00
Yusuf Cetinkaya <yck82@outlook.com>
2024-04-11 23:33:08 +0200

    Test

---
+++ PipelineDemo.Tests/Utils/MockDB.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pipeline.API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipelineDemo.Tests.Utils
+{
+    public class MockDB : IDbContextFactory<TodoDbContext>
+    {
+        public TodoDbContext CreateDbContext
+        {
+
+        }
+    }
+}
