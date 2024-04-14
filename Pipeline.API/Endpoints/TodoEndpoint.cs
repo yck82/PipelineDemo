bcaec59f873a04071f87b2d420ce629428bec890
Yusuf Cetinkaya <yck82@outlook.com>
2024-04-14 17:20:49 +0200

    Adds Tests Reporter en removed endpoints

--- Pipeline.API/Endpoints/TodoEndpoint.cs
+++ Pipeline.API/Endpoints/TodoEndpoint.cs
@@ -8,10 +8,10 @@ namespace Pipeline.API.Endpoints
         {
             app.MapGet("/todoitems", TodoHandlers.GetAllTodos);
             app.MapGet("/todoitems/complete", TodoHandlers.GetCompleteTodos);
-            app.MapGet("/todoitems/{id}", TodoHandlers.GetTodoById);
-            app.MapPost("/todoitems", TodoHandlers.CreateTodo);
-            app.MapPut("/todoitems/{id}", TodoHandlers.UpdateTodo);
-            app.MapDelete("/todoitems/{id}", TodoHandlers.DeleteTodo);
+            //app.MapGet("/todoitems/{id}", TodoHandlers.GetTodoById);
+            //app.MapPost("/todoitems", TodoHandlers.CreateTodo);
+            //app.MapPut("/todoitems/{id}", TodoHandlers.UpdateTodo);
+            //app.MapDelete("/todoitems/{id}", TodoHandlers.DeleteTodo);
         }
     }
 }
